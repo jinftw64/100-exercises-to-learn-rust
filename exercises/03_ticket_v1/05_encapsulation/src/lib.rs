ebcca3ba3391ae0fc9576f0b7736149eb3cee1ff
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2026-04-30 23:07:46 -0400

    learn encapsulation

--- exercises/03_ticket_v1/05_encapsulation/src/lib.rs
+++ exercises/03_ticket_v1/05_encapsulation/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod ticket {
+    use crate::ticket;
+
     pub struct Ticket {
         title: String,
         description: String,
@@ -34,6 +36,18 @@ pub mod ticket {
         //  - `title` that returns the `title` field.
         //  - `description` that returns the `description` field.
         //  - `status` that returns the `status` field.
+
+        pub fn title(self) -> String {
+            self.title
+        }
+
+        pub fn description(self) -> String {
+            self.description
+        }
+
+        pub fn status(self) -> String {
+            self.status
+        }
     }
 }
 
